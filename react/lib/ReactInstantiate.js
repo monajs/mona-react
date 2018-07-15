4e6baad5b64d159c3dee47c003ba043ca97e94af
yangxi <yangxi@babytree-inc.com>
2018-07-15 17:19:49 +0800

    feat: 节点挂载 && nodeType未识别问题修复

--- react/lib/ReactInstantiate.js
+++ react/lib/ReactInstantiate.js
@@ -10,13 +10,12 @@ export default class ReactInstantiate {
 		// 节点唯一key
 		this.key = key
 		// 节点类型
-		this.nodeType = reactDom.getElementType()
+		this.nodeType = reactDom.getElementType(element)
 		// 节点实例化数据结构
 		this.childrenInstance = this.instanceChildren()
-		console.log(this.childrenInstance)
 	}
 	
-	// 递归实例化所有节点
+	// 递归实例化所有节点，忽略react组件节点
 	instanceChildren () {
 		if (this.nodeType === Constant.EMPTY_NODE || this.nodeType === Constant.TEXT_NODE || !this.currentElement.props || !this.currentElement.props.children) {
 			return
@@ -49,5 +48,65 @@ export default class ReactInstantiate {
 			}
 		})
 		return childrenInstance
+	}
+	
+	// 挂载节点
+	mount (parentNode) {
+		if (this.nodeType === Constant.EMPTY_NODE) {
+			return null
+		}
+		
+		if (this.nodeType === Constant.REACT_NODE) {
+			let componentObj = new this.currentElement.type(this.currentElement.props)
+			// 获取react render()出来的真实节点信息
+			let componentElement = componentObj.render()
+			if (!componentElement) {
+				return null
+			}
+			// component实例
+			this.componentObj = componentObj
+			this.componentElement = componentElement
+			// 监听setState方法
+			this.componentObj.__events.on('stateChange', () => {
+				// this.receiveComponent()
+				// TODO
+			})
+			this.componentInstance = new ReactInstantiate(componentElement, null)
+			return this.componentInstance.mount(parentNode)
+		}
+		
+		if (!this.nativeNode) {
+			this.nativeNode = reactDom.create(this.currentElement)
+			// 事件绑定
+			// TODO
+			if (parentNode) {
+				this.parentNode = parentNode
+				parentNode.appendChild(this.nativeNode)
+			}
+		}
+		
+		this.nativeNode['__reactInstance'] = {
+			_currentElement: this.currentElement
+		}
+		
+		this.mountChildren(this.nativeNode)
+		return this.nativeNode
+	}
+	
+	// 挂载子节点
+	mountChildren (parentNode) {
+		if (!this.childrenInstance || this.childrenInstance.length === 0) {
+			return
+		}
+		this.childrenInstance.forEach((v) => {
+			if (Util.isArray(v)) {
+				v.forEach((child) => {
+					child.mount(parentNode)
+				})
+			} else {
+				console.log(parentNode)
+				v.mount(parentNode)
+			}
+		})
 	}
 }
